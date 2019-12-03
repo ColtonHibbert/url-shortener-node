5e13b0e4fd4cef7618a15fc79ef997e9f2fea11f
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-12-03 16:45:23 -0700

    added new endpoint, address validation and dns lookup validation

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
-
+const dns = require('dns');
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
@@ -15,6 +15,7 @@ var port = process.env.PORT || 3000;
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
+app.use(bodyParser.json())
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -25,6 +26,28 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/shorturl/new', function (req, res) {
+  console.log("new was pinged")
+  const input = req.body.input
+  console.log("here is the input", input)
+  const addressRegex = /https?:\/\/www./i;
+  const addressValid = input.search(addressRegex)
+  console.log("the address is", addressValid)
+  if(addressValid === -1) {
+    res.json({"error": "invalid URL"})
+    return
+  }
+  const dnsInput = input.replace(addressRegex, "")
+  console.log("here is the input after replace method", input)
+  console.log("here is the dnsinput", dnsInput)
+  dns.lookup(dnsInput, (err, address, family) => {
+    console.log(err, address, family)
+    if(err) {
+      res.json({"error": "invalid URL"})
+      return
+    }
+  })
+})
   
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
@@ -33,5 +56,5 @@ app.get("/api/hello", function (req, res) {
 
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log('Node.js listening to port', port);
 });
